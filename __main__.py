6106588b4a2c3612ac350118b661b5d7b5f8cb21
Brint O'Hearn <brintly@gmail.com>
2021-11-13 10:55:42 -0600

    Moving prod to us-east-1, adding region to bucket names for uniqueness

--- __main__.py
+++ __main__.py
@@ -1,12 +1,16 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
+import pulumi_aws as aws
 from pulumi_aws import s3
+
+# For pulling config options
+config = pulumi.Config()
 
 # Create an AWS resource (S3 Bucket)
 # Use the stack, which is the environment name for
 # bucket naming.
-bucket = s3.Bucket('%s.site' % pulumi.get_stack())
+bucket = s3.Bucket('%s.%s.site' % (aws.get_region().name, pulumi.get_stack()))
 
 # Add a file
 bucketObject = s3.BucketObject(
