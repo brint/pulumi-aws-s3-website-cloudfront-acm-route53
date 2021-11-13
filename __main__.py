ac30b597e14d6719bed35d68be53a8c62c9bac0a
Brint O'Hearn <brintly@gmail.com>
2021-11-13 10:37:08 -0600

    Using env to set bucket name

--- __main__.py
+++ __main__.py
@@ -4,7 +4,9 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# Use the stack, which is the environment name for
+# bucket naming.
+bucket = s3.Bucket('%s.site' % pulumi.get_stack())
 
 # Add a file
 bucketObject = s3.BucketObject(
