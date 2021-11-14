62d7bf382f1e38276eb2ded22445b9ef50166856
Brint O'Hearn <brintly@gmail.com>
2021-11-14 09:19:16 -0600

    Setting domain to use

--- __main__.py
+++ __main__.py
@@ -6,11 +6,19 @@ from pulumi_aws import s3
 
 # For pulling config options
 config = pulumi.Config()
+domain = config.require("domain")
 
+# Standard tags for all resources in the stack
+stack_tags = {
+    "Environment": pulumi.get_stack(),
+    "Region": aws.get_region().name,
+    "Project": domain,
+    "Orchestrator": "pulumi"
+}
 # Create an AWS resource (S3 Bucket)
 # Use the stack, which is the environment name for
 # bucket naming.
-bucket = s3.Bucket('%s.%s.site' % (aws.get_region().name, pulumi.get_stack()))
+bucket = s3.Bucket('%s.%s.%s' % (aws.get_region().name, pulumi.get_stack(), domain), acl="private", tags=stack_tags)
 
 # Add a file
 bucketObject = s3.BucketObject(
